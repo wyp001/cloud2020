d9921c77f7cc2bbb7620dd0e7b30cd05d725ed98
wyupeng <wyupeng@linewell.com>
2020-04-11 21:54:42 +0800

    17_订单服务80入进eurekaServer

--- cloud-consumer-order80/src/main/java/com/wyp/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/wyp/springcloud/OrderMain80.java
@@ -3,10 +3,11 @@ package com.wyp.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-
+@EnableEurekaClient
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration=MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
