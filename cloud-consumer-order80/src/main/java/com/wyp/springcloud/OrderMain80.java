04576d81167ce092c5a7bd817facb636acd436b5
wyupeng <wyupeng@linewell.com>
2020-04-11 21:04:49 +0800

    开启热部署，订单模块的搭建

---
+++ cloud-consumer-order80/src/main/java/com/wyp/springcloud/OrderMain80.java
@@ -0,0 +1,15 @@
+package com.wyp.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration=MySelfRule.class)
+public class OrderMain80 {
+    public static void main(String[] args) {
+            SpringApplication.run(OrderMain80.class, args);
+    }
+}
--- cloud-provider-payment8001/src/main/java/com/wyp/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/wyp/springcloud/controller/PaymentController.java
@@ -20,7 +20,7 @@ public class PaymentController
 
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment)
+    public CommonResult create(@RequestBody  Payment payment)
     {
         int result = paymentService.create(payment);
         log.info("*****插入结果："+result);
