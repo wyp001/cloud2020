38427822f1d1634f0fd96056332dd278644cd1f1
wyp <1834583115@qq.com>
2020-04-15 22:29:30 +0800

    50_Hystrix付微服务构建

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/wyp/springcloud/service/PaymentService.java
@@ -0,0 +1,35 @@
+package com.wyp.springcloud.service;
+
+import cn.hutool.core.util.IdUtil;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+
+@Service
+public class PaymentService {
+    /**
+     * 正常访问，肯定OK
+     * @param id
+     * @return
+     */
+    public String paymentInfo_OK(Integer id) {
+        return "线程池:  "+Thread.currentThread().getName()+"  paymentInfo_OK,id:  "+id+"\t"+"O(∩_∩)O哈哈~";
+    }
+
+    public String paymentInfo_TimeOut(Integer id) {
+        //int age = 10/0;
+        try {
+            TimeUnit.MILLISECONDS.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): ";
+    }
+
+
+}
