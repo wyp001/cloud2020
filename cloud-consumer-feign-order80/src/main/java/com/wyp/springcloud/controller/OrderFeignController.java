5ab909c84f62df92a50b495cb05d0825e944ff50
wyp <1834583115@qq.com>
2020-04-15 21:21:51 +0800

    45_OpenFeign超时控制

--- cloud-consumer-feign-order80/src/main/java/com/wyp/springcloud/controller/OrderFeignController.java
+++ cloud-consumer-feign-order80/src/main/java/com/wyp/springcloud/controller/OrderFeignController.java
@@ -22,4 +22,11 @@ public class OrderFeignController {
         return paymentFeignService.getPaymentById(id);
     }
 
+    @GetMapping(value = "/consumer/payment/feign/timeout")
+    public String paymentFeignTimeout()
+    {
+        // OpenFeign客户端一般默认等待1秒钟
+        return paymentFeignService.paymentFeignTimeout();
+    }
+
 }
--- cloud-provider-payment8002/src/main/java/com/wyp/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/wyp/springcloud/controller/PaymentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -74,6 +75,17 @@ public class PaymentController
     @GetMapping(value = "/payment/lb")
     public String getPaymentLB()
     {
+        return serverPort;
+    }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout() {
+        // 业务逻辑处理正确，但是需要耗费3秒钟
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return serverPort;
     }
 
