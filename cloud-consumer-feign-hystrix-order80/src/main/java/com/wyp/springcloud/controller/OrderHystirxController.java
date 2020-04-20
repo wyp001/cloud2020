a1037a949da2f3aa231aa7a4fac8dc1c3c8b1e1a
wyp <1834583115@qq.com>
2020-04-20 21:59:08 +0800

    55_Hystrix之服务降级订单侧（消费侧）fallback

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/wyp/springcloud/controller/OrderHystirxController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/wyp/springcloud/controller/OrderHystirxController.java
@@ -1,5 +1,7 @@
 package com.wyp.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.wyp.springcloud.service.PaymentHystrixService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,9 +26,24 @@ public class OrderHystirxController {
         return result;
     }
 
+    /*
+        服务要求1.5秒钟返回响应数据，如果1.5秒钟没有返回响应数据，则执行paymentTimeOutFallbackMethod方法
+        模拟情景: 8001上自身服务要去5秒钟返回数据即为正常状态，业务代码实际使用时间为3秒钟，8001自身符合要求可以正常响应数据
+        但是80端口的消费端需要1.5秒返回数据。因此服务仍然执行paymentTimeOutFallbackMethod方法
+     */
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
     }
+
+
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+        return "我是消费者80,对方支付系统繁忙请10秒种后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
+
+
 }
