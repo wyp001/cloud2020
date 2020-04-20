eb702d6e5ce4d6b25254fb17ed284ba8f705803c
wyp <1834583115@qq.com>
2020-04-20 21:39:26 +0800

    54_Hystrix之服务降级支付侧fallback_报错的情况

--- cloud-provider-hystrix-payment8001/src/main/java/com/wyp/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/wyp/springcloud/service/PaymentService.java
@@ -29,12 +29,12 @@ public class PaymentService {
             @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")
     })
     public String paymentInfo_TimeOut(Integer id) {
-        //int age = 10/0;
-        try {
-            TimeUnit.MILLISECONDS.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        int age = 10/0;
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): ";
     }
     public String paymentInfo_TimeOutHandler(Integer id)
