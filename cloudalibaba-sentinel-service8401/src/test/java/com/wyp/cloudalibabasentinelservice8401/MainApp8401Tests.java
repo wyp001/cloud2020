864694bbac1aefc42bc4b2b47e953806742905eb
wyupeng <wangyp@trgroup.cn>
2021-05-06 20:11:26 +0800

    sentinel初始化监控

---
+++ cloudalibaba-sentinel-service8401/src/test/java/com/wyp/cloudalibabasentinelservice8401/MainApp8401Tests.java
@@ -0,0 +1,13 @@
+package com.wyp.cloudalibabasentinelservice8401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApp8401Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
